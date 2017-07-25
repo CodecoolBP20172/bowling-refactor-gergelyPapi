6371f8149c9e0164b31b423d2060621d60b8ba0a
Gergely Papi <gergely.papi.cc@gmail.com>
2017-07-25 12:44:47 +0200

    Renamed bowling score variable, fixed boolan requirements, and added lists to get_value funciton.

--- bowling.py
+++ bowling.py
@@ -1,41 +1,47 @@
 def score(game):
-    result = 0
+    bowling_score = 0
     frame = 1
     in_first_half = True
     for i in range(len(game)):
         if game[i] == '/':
-            result += 10 - last
+            bowling_score += 10 - last
         else:
-            result += get_value(game[i])
-        if frame < 10  and get_value(game[i]) == 10:
+            bowling_score += get_value(game[i])
+        if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                bowling_score += get_value(game[i+1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
+                bowling_score += get_value(game[i+1])
                 if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                    bowling_score += 10 - get_value(game[i+1])
                 else:
-                    result += get_value(game[i+2])
+                    bowling_score += get_value(game[i+2])
         last = get_value(game[i])
-        if in_first_half == False:
+        if in_first_half is False:
             frame += 1
-        if in_first_half == True:
+        if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
         if game[i] == ('X') or game[i] == 'x':
             in_first_half = True
             frame += 1
-    return result
+    return bowling_score
+
+#renamed result to bowling_score
+#fixed boolean requirements
+
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    char_list = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    char_sign_list = ['X', 'x', '/']
+    if char in char_list:
         return int(char)
-    elif char == 'X' or char == 'x' or char == '/':
+    elif char in char_sign_list:
         return 10
     elif char == '-':
         return 0
     else:
         raise ValueError()
+
+#ordered get_value into lists
